c4e1917436ad35b4eaec836a29436df4ce6c7262
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-11-14 14:31:07 -0500

    week1 first try

---
+++ 01-node-tutorial/answers/01-intro.js
@@ -0,0 +1,15 @@
+const laptop = {
+    make: 'Hp',
+    model: 'Alienware',
+    memory: ['SSD', 'HDD'],
+    cores: 8,
+    memorySize: [256, 512]
+}
+
+console.log(`
+    Laptop make: ${laptop.make} \n
+    model: ${laptop.model} \n
+    memory: ${laptop.memory} \n
+    cores: ${laptop.cores} \n
+    memory size: ${laptop.memorySize} 
+    `)
