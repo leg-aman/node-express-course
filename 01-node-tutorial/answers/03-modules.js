5e92abcb4eb14c02804cd07c502749f9e8315919
leg-aman <126026416+leg-aman@users.noreply.github.com>
2025-11-14 15:31:40 -0500

    week1

---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,10 @@
+const names = require("./04-names")
+const sayHello = require('./05-utils')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+
+sayHello('Messi')
+sayHello(names.Ronaldo)
+
+console.log(data)
+console.log(names)
