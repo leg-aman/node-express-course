4a8789317189845ce010f48997703a9e9b7e2247
leg-aman <126026416+leg-aman@users.noreply.github.com>
2026-01-06 00:13:42 -0500

    week 5

---
+++ 03-task-manager/starter/controller/tasks.js
@@ -0,0 +1,31 @@
+const Task = require('../models/Task')
+
+const getAllTasks = async(req, res) => {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+}
+
+const createTask = async(req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+}
+
+const getTask = (req, res) => {
+    res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res) => {
+    res.send('task updated')
+}
+
+const deleteTask = (req, res) => {
+    res.send('task deleted')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
