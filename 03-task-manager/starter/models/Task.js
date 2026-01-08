d92b53af6c8d934b97ce3c867bc5be3ba115c93c
leg-aman <126026416+leg-aman@users.noreply.github.com>
2026-01-08 16:06:58 -0500

    week 6

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose')
 const TaskSchema = new mongoose.Schema({
     name: {
         type: String,
-        completed: Boolean,
+        require: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name can not be more than 20 characters'],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    
     },
 })
 
